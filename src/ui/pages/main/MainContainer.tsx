afdc9c5f3bea9b0b0b1dbeefcdf7e6c9d808f103
Krankenkopf <petricowork@gmail.com>
2022-04-21 05:06:11 +0300

    /localization - initial addings

--- src/ui/pages/main/MainContainer.tsx
+++ src/ui/pages/main/MainContainer.tsx
@@ -1,12 +1,17 @@
 import React, { useLayoutEffect, useState } from 'react';
 
+import { useTranslation } from 'react-i18next';
 import { NavLink, Redirect, Route, Switch, useHistory } from 'react-router-dom';
 
 import { setUserData, TUserData } from '../../../bll/reducers';
 import { selectUserData } from '../../../bll/selectors';
 import { useAppDispatch, useAppSelector } from '../../../common/hooks';
+import { getKeys } from '../../../common/utils/state';
+import { namespaces } from '../../../i18n';
+import { Button } from '../../components/elements/button/Button';
 import { Icon } from '../../components/elements/icons/Icon';
 import { TIconName } from '../../components/modules/iconSpritesMaps/IconSpritesMap';
+import translations from '../../locales/en/pages.all.json';
 import { NotFound } from '../404';
 
 import { AddonsContainer } from './addons/AddonsContainer';
@@ -26,69 +31,72 @@ type TNavLinksDataItem = {
   icon: TIconName;
 };
 
-const navLinksData: Array<TNavLinksDataItem> = [
-  {
-    id: 1,
-    title: 'Главная',
-    href: '/',
-    icon: 'home',
-  },
-  {
-    id: 2,
-    title: 'Кросспостинг',
-    href: '/crossposting',
-    icon: 'grid-2',
-  },
-  {
-    id: 3,
-    title: 'Мой сайт',
-    href: '/my-site',
-    icon: 'window',
-  },
-  {
-    id: 4,
-    title: 'Магазин',
-    href: '/shop',
-    icon: 'cart',
-  },
-  {
-    id: 5,
-    title: 'Аналитика',
-    href: '/analytics',
-    icon: 'analytics',
-  },
-  {
-    id: 6,
-    title: 'Дополнительно',
-    href: '/addons',
-    icon: 'grid-mosaic',
-  },
-  {
-    id: 7,
-    title: 'Тарифы',
-    href: '/price',
-    icon: 'lightning',
-  },
-  {
-    id: 10,
-    title: 'Чат',
-    href: '/chat',
-    icon: 'chat',
-  },
-];
-
 export const MainContainer = () => {
   const dispatch = useAppDispatch();
   const history = useHistory();
   const userData = useAppSelector<TUserData | null>(selectUserData);
 
+  const { t, i18n } = useTranslation('pages');
+  const changeLanguage = (language: string) => {
+    i18n.changeLanguage(language);
+  };
   useLayoutEffect(() => {
     const panels: any = document.getElementsByClassName('panel');
     for (let i = 0; i < panels.length; i++) {
       panels[i].style.background = `#f${Math.random().toString(16).substr(-4)}f`;
     }
   }, [history.location.pathname]);
-
+  const navLinksData: Array<TNavLinksDataItem> = [
+    {
+      id: 1,
+      title: t('navbar.home'),
+      href: '/',
+      icon: 'home',
+    },
+    {
+      id: 2,
+      title: t('navbar.crossposting'),
+      href: '/crossposting',
+      icon: 'grid-2',
+    },
+    {
+      id: 3,
+      title: t('navbar.multilink'),
+      href: '/my-site',
+      icon: 'window',
+    },
+    {
+      id: 4,
+      title: t('navbar.shop'),
+      href: '/shop',
+      icon: 'cart',
+    },
+    {
+      id: 5,
+      title: t('navbar.analytics'),
+      href: '/analytics',
+      icon: 'analytics',
+    },
+    {
+      id: 6,
+      title: t('navbar.addons'),
+      href: '/addons',
+      icon: 'grid-mosaic',
+    },
+    {
+      id: 7,
+      title: t('navbar.price'),
+      href: '/price',
+      icon: 'lightning',
+    },
+    {
+      id: 10,
+      title: t('navbar.chat'),
+      href: '/chat',
+      icon: 'chat',
+    },
+  ];
+  const titles = getKeys(t('navbar'));
   const mappedNavLinks = navLinksData.map((data, i) => (
     <li key={data.id} className="nav__item">
       <NavLink to={data.href} className="nav__link iconized__L" exact>
@@ -118,6 +126,8 @@ export const MainContainer = () => {
           <nav className="nav">
             <ul className="nav__list">{mappedNavLinks}</ul>
           </nav>
+          <Button onClick={() => changeLanguage('en')}>EN</Button>
+          <Button onClick={() => changeLanguage('ru')}>RU</Button>
           <section className="profile-link">
             <NavLink to="/profile" className="iconized__L" exact>
               <Icon name="user" size="full" />
