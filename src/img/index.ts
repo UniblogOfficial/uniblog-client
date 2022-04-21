d79cf8ae33b44f727323ef36e5e457e3e3415d55
Krankenkopf <petricowork@gmail.com>
2022-04-22 00:19:49 +0300

    /layout - homepage layout expanded

--- src/img/index.ts
+++ src/img/index.ts
@@ -1,10 +1,50 @@
+type TImage = {
+  title: string;
+  src: any;
+};
+
 const importAll = (r: any) => {
-  const images = {} as any;
+  const images = [] as Array<TImage>;
   r.keys().map((item: string, i: number) => {
-    images[item.replace('./', '').split('.')[0]] = r(item).default;
+    images.push({ title: item.replace('./', '').split('.')[0], src: r(item).default });
   });
   console.log(images);
   return images;
 };
+// important! this titles must be the same to filenames
+const socialsPopularitySequence = [
+  'instagram',
+  'tiktok',
+  'telegram',
+  'vk',
+  'youtube',
+  'facebook',
+  'twitter',
+  'pinterest',
+];
+// important! this titles will be shown in front!
+const socialsFormattedPopularitySequence = [
+  'Instagram',
+  'TikTok',
+  'Telegram',
+  'Vk',
+  'Youtube',
+  'Facebook',
+  'Twitter',
+  'Pinterest',
+];
 
-export const socials = importAll(require.context('./socials/', false, /\.(png|jpe?g|svg)$/));
+const socials = importAll(require.context('./socials/', false, /\.(png|jpe?g|svg)$/));
+
+export default socials
+  // sort according to popularity
+  .sort(
+    (a: TImage, b: TImage) =>
+      socialsPopularitySequence.indexOf(a.title.toLowerCase()) -
+      socialsPopularitySequence.indexOf(b.title.toLowerCase()),
+  )
+  // format titles according to socialsFormattedPopularitySequence
+  .map((social: TImage, i: number) => ({
+    ...social,
+    title: socialsFormattedPopularitySequence[i],
+  }));
--- src/ui/pages/main/home/HomeContainer.tsx
+++ src/ui/pages/main/home/HomeContainer.tsx
@@ -1,30 +1,36 @@
 import React from 'react';
 
+import { useTranslation } from 'react-i18next';
+
 import { TUserData } from '../../../../bll/reducers';
 import { selectUserData } from '../../../../bll/selectors';
 import { useAppSelector } from '../../../../common/hooks';
-import { socials } from '../../../../img';
+import socials from '../../../../img';
 import { Button } from '../../../components/elements/button/Button';
+import { SocialCard } from '../../../components/modules/socialCard/SocialCard';
 
 type THomeContainerProps = {};
 
 export const HomeContainer = () => {
   const userData = useAppSelector<TUserData | null>(selectUserData);
+  const { t } = useTranslation(['pages', 'common']);
   if (!userData) return null;
   console.log(socials);
 
-  const images = Object.keys(socials).map((name: string) => (
-    <li key={socials.name}>
-      <img src={socials[name]} alt={name} />
+  const images = socials.map((social: any) => (
+    <li key={social.title}>
+      <SocialCard data={social} titleChange={t('common:links.change', { ns: 'common' })} />
     </li>
   ));
   return (
     <div className="home">
       <header className="home__header">
-        <h1 className="page-title home__greeting">Привет, {userData.name}</h1>
+        <h1 className="page-title home__greeting">
+          {t('pages:home.greeting')}, {userData.name}
+        </h1>
         <div className="home__add-new-post">
           <Button>
-            Добавить новый пост <strong>+</strong>
+            {t('common:buttons.addNewPost', { ns: 'common' })} <strong>+</strong>
           </Button>
         </div>
       </header>
@@ -32,7 +38,7 @@ export const HomeContainer = () => {
         <div className="grid__row row-2">
           <section className="r-paper chart">11</section>
           <section className="paper socials">
-            <h3 className="paper-title">Мои соцсети</h3>
+            <h3 className="paper-title">{t('pages:home.subtitles.myNetworks')}</h3>
             <ul className="socials__grid">{images}</ul>
           </section>
         </div>
