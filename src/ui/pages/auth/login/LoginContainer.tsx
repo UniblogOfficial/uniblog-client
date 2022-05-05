5f9c887f64ddd1d335bfa28c5e3b3517cab99770
Krankenkopf <petricowork@gmail.com>
2022-05-05 18:31:37 +0300

    auth: add api request handlers

--- src/ui/pages/auth/login/LoginContainer.tsx
+++ src/ui/pages/auth/login/LoginContainer.tsx
@@ -1,7 +1,19 @@
 import React, { FC } from 'react';
 
+import { Redirect } from 'react-router-dom';
+
+import { TUserData } from '../../../../bll/reducers';
+import { selectUserData } from '../../../../bll/selectors';
+import { useAppSelector } from '../../../../common/hooks';
+
 import { Login } from './Login';
 
 type TLoginContainerProps = {};
 
-export const LoginContainer: FC<TLoginContainerProps> = () => <Login />;
+export const LoginContainer: FC<TLoginContainerProps> = () => {
+  const userData = useAppSelector<TUserData | null>(selectUserData);
+  if (userData !== null) {
+    return <Redirect to="/" />;
+  }
+  return <Login />;
+};
