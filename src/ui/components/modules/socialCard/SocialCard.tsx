108869fdbdfbded8084af47413fed5a0de4d8168
Krankenkopf <petricowork@gmail.com>
2022-05-12 09:03:56 +0300

    socialCards: add modals

--- src/ui/components/modules/socialCard/SocialCard.tsx
+++ src/ui/components/modules/socialCard/SocialCard.tsx
@@ -1,23 +1,37 @@
-import React, { FC, MouseEvent } from 'react';
+import React, { FC, MouseEvent, useState } from 'react';
+
+import { useTranslation } from 'react-i18next';
+
+import { Button, Icon } from '../../elements';
+import { Modal } from '../modals/Modal';
 
 import styles from './SocialCard.module.scss';
+
+export enum SocialAccountState {
+  NOT_AVAILABLE = 0,
+  CHECKED = 1,
+  UNCHECKED = 2,
+}
 
 type TSocialCardProps = {
   data: {
     title: string;
     src: string;
   };
-  titleChange: string;
-  state?: 'checked' | 'unchecked' | 'not available';
+  actionTitle: string;
+  state?: SocialAccountState;
   className?: string;
 };
 
 export const SocialCard: FC<TSocialCardProps> = ({
   data,
-  titleChange,
-  state = 'not available',
+  actionTitle,
+  state = SocialAccountState.NOT_AVAILABLE,
   className,
 }) => {
+  const { t } = useTranslation(['pages', 'common']);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   const host =
     process.env.REACT_APP_MODE === 'production'
       ? process.env.REACT_APP_HOST_PRODUCTION
@@ -27,28 +41,43 @@ export const SocialCard: FC<TSocialCardProps> = ({
   const cbLink = `https://oauth.vk.com/blank.html`;
   const scope = 'offline,groups,audio,video,wall,photos,email';
   const changeLink = `https://oauth.vk.com/authorize?client_id=${process.env.REACT_APP_VK_CLIENT_ID}&display=page&redirect_uri=${cbLink}&scope=${scope}&response_type=token&v=v=5.85&revoke=1&state=new`;
-  const onChangeSocialClick = (e: MouseEvent<HTMLButtonElement>) => {
+  const onActionButtonClick = (e: MouseEvent<HTMLButtonElement>) => {
+    setIsModalVisible(true);
     // window.location.href = `https://oauth.vk.com/authorize?client_id=${process.env.REACT_APP_VK_CLIENT_ID}&display=popup&redirect_uri=${cbLink}&scope=${scope}&response_type=token&v=v=5.85&revoke=1&state=new`;
     // window.open(changeLink, '_blank', 'width=600,height=400');
-    window.addEventListener('message', ev => {
-      console.log(ev.origin);
-    });
     // @ts-ignore
-    const authWindow = document.open(changeLink, '_blank', 'width=777,height=666');
+    // const authWindow = document.open(changeLink, '_blank', 'width=777,height=666');
   };
 
   const finalClassName = className ? `${styles.socialCard} ${className}` : styles.socialCard;
   return (
-    <article className={finalClassName}>
-      <div className={styles.imgContainer}>
-        <img src={data.src} alt={data.title} />
-      </div>
-      <div className={styles.title}>
-        <h4>{data.title}</h4>
-        <button type="button" onClick={onChangeSocialClick}>
-          {titleChange}
-        </button>
-      </div>
-    </article>
+    <>
+      <article className={finalClassName}>
+        <div className={styles.imgContainer}>
+          <img src={data.src} alt={data.title} />
+        </div>
+        <div className={styles.title}>
+          <h4>{data.title}</h4>
+          <button type="button" onClick={onActionButtonClick}>
+            {actionTitle}
+          </button>
+        </div>
+        <Icon name="circle-add" />
+      </article>
+      {isModalVisible && (
+        <Modal close={() => setIsModalVisible(false)}>
+          <div className="paper _with-button-bottom">
+            <div className={styles.imgContainer}>
+              <img src={data.src} alt={data.title} />
+            </div>
+            <div className="paper__button-container">
+              <Button value="1" className="button _full _paper">
+                {t('common:buttons.ok')}
+              </Button>
+            </div>
+          </div>
+        </Modal>
+      )}
+    </>
   );
 };
