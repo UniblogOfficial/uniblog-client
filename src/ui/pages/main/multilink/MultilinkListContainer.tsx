d7aa4c1b9a425d44792f63974c09e9e95bb2e615
Krankenkopf <petricowork@gmail.com>
2022-05-22 18:59:35 +0300

    ml.list: initial wrap

--- src/ui/pages/main/multilink/MultilinkListContainer.tsx
+++ src/ui/pages/main/multilink/MultilinkListContainer.tsx
@@ -1,5 +1,84 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { getAllMultilinks } from '../../../../bll/reducers';
+import { ID, MLContentType } from '../../../../common/constants';
+import { useAppDispatch, useAppSelector } from '../../../../common/hooks';
+import { Nullable, TMLContent, TMultilink } from '../../../../common/types/instance';
+import { parseRawImage } from '../../../../common/utils/ui';
+import phone from '../../../../img/phone.png';
+import { Icon } from '../../../components/elements';
 
 type TMultilinkListContainerProps = {};
 
-export const MultilinkListContainer = () => <></>;
+export const MultilinkListContainer = () => {
+  const dispatch = useAppDispatch();
+  const multilinks = useAppSelector<Nullable<TMultilink[]>>(state => state.multilink.allMultilinks);
+  useEffect(() => {
+    dispatch(getAllMultilinks());
+  }, [dispatch]);
+  const mappedMLs =
+    multilinks &&
+    multilinks.map(({ name, logo, template, background, content }) => {
+      const getBlockLayout = (cont: TMLContent) => {
+        switch (cont.type) {
+          case MLContentType.LINK:
+            return (
+              cont.link && (
+                <a
+                  href={cont.link || undefined}
+                  target="_blank"
+                  className="link link-nulled"
+                  rel="noreferrer">
+                  {cont.title}
+                </a>
+              )
+            );
+          case MLContentType.TEXT:
+            return <p className="text">{cont.text}</p>;
+          case MLContentType.IMAGE:
+            return <img src={parseRawImage(cont)} alt="img" />;
+          default:
+            return null;
+        }
+      };
+
+      const contentLayout = (
+        <>
+          {template.map((temp, i) => (
+            <div
+              key={ID[i]}
+              className="public__content-block"
+              style={{
+                flex: `0 1 ${temp}%`,
+              }}>
+              {getBlockLayout(content.find(block => block.order === i) || content[i])}
+            </div>
+          ))}
+        </>
+      );
+      const logoSrc = logo ? parseRawImage(logo) : undefined;
+      return (
+        <section key={name} className="preview-area">
+          <div className="paper">
+            <div className="preview-device">
+              <div className="phone">
+                <div className="phone__container" style={{ background }}>
+                  <div className="phone__logo">
+                    {logoSrc ? <img src={logoSrc} alt="logo" /> : <Icon name="user" />}
+                  </div>
+                  <h4 className="phone__user-title">
+                    <strong>{name}</strong>
+                  </h4>
+                  <div className="phone__template">{contentLayout}</div>
+                </div>
+                <div className="phone__layout">
+                  <img src={phone} alt="phone-layout" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      );
+    });
+  return <div className="grid__row">{mappedMLs}</div>;
+};
