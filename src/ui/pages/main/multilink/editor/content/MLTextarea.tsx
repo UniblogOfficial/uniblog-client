4e59d45d1c97d25d147d1e31985f1bdf8a1d61a2
Moon <m.p.skuratova@gmail.com>
2022-05-31 15:21:19 +0600

    ml.texterea: add useDebounce

--- src/ui/pages/main/multilink/editor/content/MLTextarea.tsx
+++ src/ui/pages/main/multilink/editor/content/MLTextarea.tsx
@@ -1,7 +1,8 @@
-import React, { ChangeEvent, KeyboardEvent } from 'react';
+import React, { ChangeEvent, KeyboardEvent, useEffect, useState } from 'react';
 
 import { setMLDraftTextBlockContent } from '../../../../../../bll/reducers';
 import { useAppDispatch } from '../../../../../../common/hooks';
+import { useDebounce } from '../../../../../../common/hooks/useDebounce.';
 import { IMLDraftContentText, Nullable } from '../../../../../../common/types/instance';
 import { Textarea } from '../../../../../components/elements';
 
@@ -12,21 +13,28 @@ type TMLTextareaProps = {
 
 export const MLTextarea = ({ order, block }: TMLTextareaProps) => {
   const dispatch = useAppDispatch();
+  const [text, setText] = useState(block?.text ?? '');
+  const debouncedValue = useDebounce(text, 500);
   const onTextareaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    const text = e.target.value;
+    const newText = e.target.value;
     if (!block) {
       return;
     }
-    block.text = text;
-    dispatch(setMLDraftTextBlockContent(block, order));
+    setText(newText);
   };
+  useEffect(() => {
+    if (block) {
+      block.text = debouncedValue;
+      dispatch(setMLDraftTextBlockContent(block, order));
+    }
+  }, [debouncedValue]);
 
   if (!block) return null;
 
   return (
     <Textarea
       data-value={order}
-      value={block.text ?? ''}
+      value={text}
       onChange={onTextareaChange}
       maxLength={1023}
       className="textarea"
