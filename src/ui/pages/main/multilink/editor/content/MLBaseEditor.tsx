48f06600ce010229545da925035b7fbdbe523e5e
AnnaChernyshova <ann020894@gmail.com>
2022-06-17 11:49:07 +0300

    MLTextEditor: fixed

--- src/ui/pages/main/multilink/editor/content/MLBaseEditor.tsx
+++ src/ui/pages/main/multilink/editor/content/MLBaseEditor.tsx
@@ -30,63 +30,63 @@ export const MLBaseEditor = ({ blockEditor }: TMLBaseEditorProps) => {
     dispatch(setMLDraftBlockContent(block, order, 'textBlocks'));
   };
   const onPaddingChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const padding = e.currentTarget.value;
+    const padding = +e.currentTarget.value;
     const title = e.currentTarget.name;
 
     if (title === 'top') {
       if (isPaddingTopBottom) {
-        block.padding[2] = +padding;
+        block.padding[2] = padding;
       }
-      block.padding[0] = +padding;
+      block.padding[0] = padding;
     }
     if (title === 'right') {
       if (isPaddingLeftRight) {
-        block.padding[3] = +padding;
+        block.padding[3] = padding;
       }
-      block.padding[1] = +padding;
+      block.padding[1] = padding;
     }
     if (title === 'bottom') {
       if (isPaddingTopBottom) {
-        block.padding[0] = +padding;
+        block.padding[0] = padding;
       }
-      block.padding[2] = +padding;
+      block.padding[2] = padding;
     }
     if (title === 'left') {
       if (isPaddingLeftRight) {
-        block.padding[1] = +padding;
+        block.padding[1] = padding;
       }
-      block.padding[3] = +padding;
+      block.padding[3] = padding;
     }
     dispatch(setMLDraftBlockContent(block, order, 'textBlocks'));
   };
 
   const onMarginChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const margin = e.currentTarget.value;
+    const margin = +e.currentTarget.value;
     const title = e.currentTarget.name;
 
     if (title === 'top') {
       if (isMarginTopBottom) {
-        block.margin[2] = +margin;
+        block.margin[2] = margin;
       }
-      block.margin[0] = +margin;
+      block.margin[0] = margin;
     }
     if (title === 'right') {
       if (isMarginLeftRight) {
-        block.margin[3] = +margin;
+        block.margin[3] = margin;
       }
-      block.margin[1] = +margin;
+      block.margin[1] = margin;
     }
     if (title === 'bottom') {
       if (isMarginTopBottom) {
-        block.margin[0] = +margin;
+        block.margin[0] = margin;
       }
-      block.margin[2] = +margin;
+      block.margin[2] = margin;
     }
     if (title === 'left') {
       if (isMarginLeftRight) {
-        block.margin[1] = +margin;
+        block.margin[1] = margin;
       }
-      block.margin[3] = +margin;
+      block.margin[3] = margin;
     }
 
     dispatch(setMLDraftBlockContent(block, order, 'textBlocks'));
