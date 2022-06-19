438f5182534cd79a1ad51fa5ff5602108b1425f7
Krankenkopf <petricowork@gmail.com>
2022-06-19 22:57:32 +0300

    ml.link: add block logic

--- src/ui/components/modules/mlBlocks/mlLink/MLLink.tsx
+++ src/ui/components/modules/mlBlocks/mlLink/MLLink.tsx
@@ -5,24 +5,45 @@ import { px } from 'common/utils/ui';
 
 type TMLLinkProps = {
   block: Nullable<IMLDraftLink>;
+  isPublic?: boolean;
   callback?: <T>(payload: T) => void;
 };
 
-export const MLLink = ({ block, callback }: TMLLinkProps) => {
+export const MLLink = ({ block, isPublic, callback }: TMLLinkProps) => {
   if (!block) return null;
   const className = callback ? 'ml-link interactive' : 'ml-link';
+  const style = {
+    padding: px(block.padding) ?? '0',
+    textAlign: block.align,
+    fontSize: block.fontSize,
+    fontWeight: block.fontWeight,
+    fontStyle: block.fontStyle,
+    fontVariant: block.fontVariant,
+    lineHeight: block.lineHeight,
+    fontFamily: block.fontFamily,
+    font: block.font,
+    color: block.color,
+    background: block.background,
+    letterSpacing: block.letterSpacing,
+    textShadow: block.textShadow?.join('px '),
+  };
   return (
     <section
       className={className}
       style={{
-        padding: px(block.padding) ?? '0',
         margin: px(block.margin) ?? '0',
-        background: block.background ?? undefined,
+        justifyContent: block.align,
       }}>
       {callback && (
         <input type="button" data-type={block.type} data-order={block.order} onClick={callback} />
       )}
-      <div style={{ fontSize: block.fontSize ?? undefined }}>{block.title}</div>
+      {isPublic ? (
+        <a href={block.href ?? '#'} style={style}>
+          {block.title}
+        </a>
+      ) : (
+        <div style={style}>{block.title}</div>
+      )}
     </section>
   );
 };
