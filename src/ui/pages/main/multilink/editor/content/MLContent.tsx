8bbc511d1ef6a49749664d3bad323a17b483b71e
SashaPolyanski <80836080+SashaPolyanski@users.noreply.github.com>
2022-06-28 20:46:02 +0300

    MLContent: add Alphabet from nanoid and add case

--- src/ui/pages/main/multilink/editor/content/MLContent.tsx
+++ src/ui/pages/main/multilink/editor/content/MLContent.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, MouseEvent, useState, useMemo } from 'react';
 
-import { nanoid } from '@reduxjs/toolkit';
+import { customAlphabet } from 'nanoid';
 import { useTranslation } from 'react-i18next';
 
 import { MLButtonEditor } from './MLButtonEditor/MLButtonEditor';
@@ -16,7 +16,18 @@ import { withBaseEditor } from './withBaseEditor';
 import { addMLDraftBlock } from 'bll/reducers';
 import { MLContentType } from 'common/constants';
 import { useAppDispatch } from 'common/hooks';
-import { Nullable, TImageFile, TMLDraftBlocks, TMLDraftImages } from 'common/types/instance';
+import {
+  MLDraftButton,
+  MLDraftImage,
+  MLDraftImageText,
+  MLDraftShop,
+  MLDraftSocial,
+  MLDraftWidget,
+  Nullable,
+  TImageFile,
+  TMLDraftBlocks,
+  TMLDraftImages,
+} from 'common/types/instance';
 import {
   MLDraftLink,
   MLDraftLogo,
@@ -56,7 +67,12 @@ export const MLContent = (props: TMLContentProps) => {
         setBlockEditor(null);
         return;
       }
+      const nanoid = customAlphabet(
+        '-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+        10,
+      );
       const id = nanoid();
+      console.log(id);
       dispatch(addMLDraftBlock({ type: e.currentTarget.value as MLContentType, id }));
       setBlockEditor({ type: e.currentTarget.value as MLContentType, id });
     },
@@ -184,14 +200,11 @@ export const MLContent = (props: TMLContentProps) => {
     switch (blockEditorType) {
       case MLContentType.LOGO: {
         if (currentBlock instanceof MLDraftLogo) {
-          return (
-            currentBlock &&
-            withBaseEditor({
-              id: blockEditorId,
-              block: currentBlock,
-              images: images.blocks[blockEditorType][order],
-            })(MLLogoEditor)
-          );
+          return withBaseEditor({
+            id: blockEditorId,
+            block: currentBlock,
+            images: images.blocks[blockEditorType][order],
+          })(MLLogoEditor);
         }
         break;
       }
@@ -204,79 +217,71 @@ export const MLContent = (props: TMLContentProps) => {
         }
         break;
       }
-      /* case MLContentType.IMAGETEXT: {
-        const currentBlock = blocks[blockEditorType][blockEditorId];
-        return currentBlock && <>Not implemented</>;
+      case MLContentType.IMAGETEXT: {
+        if (currentBlock instanceof MLDraftImageText) {
+          return <>Not implemented</>;
+        }
+        break;
       }
       case MLContentType.IMAGE: {
-        const currentBlock = blocks[blockEditorType][blockEditorId];
-        return (
-          currentBlock && (
-            <>
-              <MLImageEditor
-                order={blockEditorId}
-                block={currentBlock}
-                images={images.blocks[blockEditorType][blockEditorId]}
-              />
-            </>
-          )
-        );
-      } */
+        if (currentBlock instanceof MLDraftImage) {
+          return withBaseEditor({
+            id: blockEditorId,
+            block: currentBlock,
+            images: images.blocks[blockEditorType][order],
+          })(MLImageEditor);
+        }
+        break;
+      }
       case MLContentType.LINK: {
-        const image = images.blocks[blockEditorType][order];
-
         if (currentBlock instanceof MLDraftLink) {
           return withBaseEditor({
             id: blockEditorId,
             block: currentBlock,
-            image,
+            image: images.blocks[blockEditorType][order],
             close: onButtonEditorClick,
           })(MLLinkEditor);
         }
         break;
       }
 
-      /* case MLContentType.BUTTON: {
-        const currentBlock = blocks[blockEditorType][blockEditorId];
-        return (
-          currentBlock &&
-          withBaseEditor({
-            order: blockEditorId,
+      case MLContentType.BUTTON: {
+        if (currentBlock instanceof MLDraftButton) {
+          return withBaseEditor({
+            id: blockEditorId,
             block: currentBlock,
-          })(MLButtonEditor)
-        );
+          })(MLButtonEditor);
+        }
+        break;
       }
       case MLContentType.SOCIAL: {
-        const currentBlock = blocks[blockEditorType][blockEditorId];
-        return (
-          currentBlock &&
-          withBaseEditor({
-            order: blockEditorId,
+        if (currentBlock instanceof MLDraftSocial) {
+          return withBaseEditor({
+            id: blockEditorId,
             block: currentBlock,
-          })(null)
-        );
+          })(null);
+        }
+        break;
       }
       case MLContentType.WIDGET: {
-        const currentBlock = blocks[blockEditorType][blockEditorId];
-        return (
-          currentBlock &&
-          withBaseEditor({
-            order: blockEditorId,
+        if (currentBlock instanceof MLDraftWidget) {
+          return withBaseEditor({
+            id: blockEditorId,
             block: currentBlock,
-          })(MLWidgetEditor)
-        );
+          })(MLWidgetEditor);
+        }
+        break;
       }
       case MLContentType.SHOP: {
-        const currentBlock = blocks[blockEditorType][blockEditorId];
-        return (
-          currentBlock &&
-          withBaseEditor({
-            order: blockEditorId,
+        if (currentBlock instanceof MLDraftShop) {
+          return withBaseEditor({
+            id: blockEditorId,
             block: currentBlock,
-            images: images.blocks[blockEditorType][blockEditorId],
-          })(MLShopEditor)
-        );
-      } */
+            images: images.blocks[blockEditorType][order],
+          })(MLShopEditor);
+        }
+        break;
+      }
       case MLContentType.MAP: {
         if (currentBlock instanceof MLDraftMap) {
           return withBaseEditor({
