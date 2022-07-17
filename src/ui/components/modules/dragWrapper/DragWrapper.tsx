ade10ba89d122c0612ffd33c6d95e6d40676d973
Krankenkopf <petricowork@gmail.com>
2022-07-17 16:22:19 +0300

    modules: dragWrapper hotfix (3)

---
+++ src/ui/components/modules/dragWrapper/DragWrapper.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from 'react';
+
+import { Draggable } from 'react-beautiful-dnd';
+
+import { DragButton } from './dragButton/DragButton';
+
+import { getDraggedBlockStyle } from 'common/utils/ui/styleAssemblers';
+
+type TDragWrapperProps = {
+  id: string;
+  index: number;
+  isVisible: boolean;
+};
+
+export const DragWrapper: FC<TDragWrapperProps> = ({ id, index, isVisible, children }) => {
+  if (!isVisible) {
+    return <>{children}</>;
+  }
+
+  return (
+    <Draggable draggableId={id} index={index}>
+      {({ draggableProps, innerRef, dragHandleProps }, { isDragging }) => (
+        <section
+          ref={innerRef}
+          {...draggableProps}
+          {...dragHandleProps}
+          style={getDraggedBlockStyle(isDragging, draggableProps.style)}>
+          <div style={{ position: 'relative' }}>
+            <DragButton />
+            {children}
+          </div>
+        </section>
+      )}
+    </Draggable>
+  );
+};
