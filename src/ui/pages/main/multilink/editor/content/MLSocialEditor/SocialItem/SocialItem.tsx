22d8dc884e48408ea0bf56890be051b0718d92b0
ole4ka140592 <ole4ka140592@gmail.com>
2022-07-26 15:34:10 +0300

    add changes: -create component SocialItem -create linkTypeElements, socialElements

---
+++ src/ui/pages/main/multilink/editor/content/MLSocialEditor/SocialItem/SocialItem.tsx
@@ -0,0 +1,44 @@
+import React, { ChangeEvent, useState } from 'react';
+
+import styles from './SocialItem.module.scss';
+
+type SocialItemPropsType = {
+  socialNetwork: string;
+};
+
+export const SocialItem = ({ socialNetwork }: SocialItemPropsType) => {
+  const [socNetwork, setSocNetwork] = useState<string>(socialNetwork);
+  const [errorSocialNetwork, setSocialNetwork] = useState<string | null>('');
+  const [socialNetworkTouch, setSocialNetworkTouch] = useState(false);
+
+  const onChangeInputValue = (e: ChangeEvent<HTMLInputElement>) => {
+    setSocNetwork(e.currentTarget.value);
+    const objRE =
+      /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+    if (!objRE.test(String(e.currentTarget.value))) {
+      setSocialNetwork('Invalid URL');
+    } else {
+      setSocialNetwork('');
+    }
+  };
+
+  const isEditMode = () => {
+    setSocialNetworkTouch(true);
+  };
+  return (
+    <>
+      <input
+        value={socNetwork}
+        type="text"
+        name="email"
+        placeholder="Enter your url"
+        onChange={onChangeInputValue}
+        onBlur={isEditMode}
+        className={styles.socNetwork}
+      />
+      {socialNetworkTouch && errorSocialNetwork && (
+        <div className={styles.errorSocialNetwork}>{errorSocialNetwork}</div>
+      )}
+    </>
+  );
+};
